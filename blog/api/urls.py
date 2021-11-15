8685eb783d13cfee1d3a4462189765cafe9a60c3
serhiion <onyserhii@gmail.com>
2021-11-15 16:20:10 +0200

    fix rest

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .api_views import PostListAPIView
 
 urlpatterns = [
-    path('posts/', PostListAPIView.as_view(), name='post')
+    path('', PostListAPIView.as_view(), name='post')
 ]
--- user/app/urls.py
+++ user/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .api_views import ProfileListAPIView
 
 urlpatterns = [
-    path('profiles/', ProfileListAPIView.as_view(), name='post')
+    path('', ProfileListAPIView.as_view(), name='post')
 ]
